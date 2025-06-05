bfafcc0222fbbc6ab9066f602b9788ef732aa40c
yousefghawi <yousefghawi13@gmail.com>
2025-06-05 14:38:16 +0300

    fix

--- src/component/Conformation/Conformation.jsx
+++ src/component/Conformation/Conformation.jsx
@@ -1,32 +1,81 @@
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, } from '@mui/material';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
+import { useState } from "react";
 
 const ConfirmDialog = ({
-    open,
-    title = "Confirm Action",
-    description = "This action cannot be undone.",
-    onClose,
-    onConfirm,
+  open,
+  title = "Confirm Action",
+  description = "This action cannot be undone.",
+  onClose,
+  onConfirm,
 }) => {
-    return (
-        <Dialog open={open} onClose={onClose} sx={{
-            '& .MuiDialog-paper': {
-                borderRadius: 4,
-                padding: 2,
-                backgroundColor: '#fff',
-                minWidth: 300,
-                minHeight:200
-            },
-        }}>
-            <DialogTitle sx={{ fontWeight: 'bold', fontSize: 20, textAlign:"center"}}>{title}</DialogTitle>
-            <DialogContent sx={{ fontSize: 16}}>
-                <Typography>{description}</Typography>
-            </DialogContent>
-            <DialogActions sx={{ justifyContent: 'space-evenly', gap: 1  }}>
-                <Button onClick={onClose} variant="outlined" color="inherit" sx={{padding:1}}> Cancel </Button>
-                <Button onClick={onConfirm} variant="contained" color="success" sx={{padding:1}}> Confirm </Button>
-            </DialogActions>
-        </Dialog>
-    );
+  const [loading, setLoading] = useState(false);
+
+  const handleConfirm = async () => {
+    setLoading(true);
+    try {
+      await onConfirm();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      sx={{
+        "& .MuiDialog-paper": {
+          borderRadius: 4,
+          padding: 2,
+          backgroundColor: "#fff",
+          minWidth: 300,
+          minHeight: 200,
+        },
+      }}
+    >
+      <DialogTitle
+        sx={{ fontWeight: "bold", fontSize: 20, textAlign: "center" }}
+      >
+        {title}
+      </DialogTitle>
+      <DialogContent sx={{ fontSize: 16 }}>
+        <Typography>{description}</Typography>
+      </DialogContent>
+      <DialogActions sx={{ justifyContent: "space-evenly", gap: 1 }}>
+        <Button
+          onClick={onClose}
+          variant="outlined"
+          sx={{ padding: 1,
+            background:"#723d46",
+            color:"white",
+           }}
+          disabled={loading}
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={handleConfirm}
+          disabled={loading}
+          variant="contained"
+          sx={{
+            padding:1,
+            background:"#723d46",
+            color:"white",
+          }}
+        >
+          {loading ? <CircularProgress size={20} color={"#723d46"} /> : "Confirm"}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
 
 export default ConfirmDialog;
